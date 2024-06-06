f921d40fa24f667dc213b9b6509970c025550672
YeganeGholipour <yeganegholiour@gmail.com>
2024-06-07 01:11:54 +0330

    add : .env file

--- cat_diary/settings.py
+++ cat_diary/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hgph+eljnbhm@gf!-r10zaqe5ss+^q-k6*$a@1f7@@uu&vp=-0"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
